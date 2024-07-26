fb018d3dc76da679622bd0ea45a17fc158050236
bardiafar <bardiafarokhsereshti@gmail.com>
2024-07-26 13:43:08 +0330

    cleaning the js code

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 const addButton = document.getElementById('addButton');
-const deleteButton = document.getElementById('deleteButton');
-const editButton = document.getElementById('editButton');
-const applyButton = document.getElementById('applyButton');
-const completeButton = document.getElementById('completeButton');
 const taskInput = document.getElementById('taskInput');
-const editInput = document.getElementById('editInput');
 const taskList = document.getElementById('taskList')
 
 
@@ -15,44 +10,6 @@ window.onload = function() {
   }
 
 };
-
-function getNumber(){
-  let number = JSON.parse(sessionStorage.getItem('highNumber')) || 0;
-  number = parseInt(number)
-  return number + 1 ;
-}
-
-function saveTask(task,number){
-    let tasks = JSON.parse(sessionStorage.getItem('tasks')) || {};
-    tasks[number]= task;
-    sessionStorage.setItem('tasks',JSON.stringify(tasks));
-    sessionStorage.setItem('highNumber',JSON.stringify(number));
-    number++;
-    
-}
-
-function getTask(number) {
-    let tasks = JSON.parse(sessionStorage.getItem('tasks'));
-    let taskKey = String(number); 
-    return tasks[taskKey];
-  }
-
-function delTask(number){
-  let tasks = JSON.parse(sessionStorage.getItem('tasks'));
-  delete tasks[number];
-  sessionStorage.setItem('tasks',JSON.stringify(tasks));
-
-}
-  
-addButton.addEventListener('click', addInput);
-function addInput() {
-    let task = taskInput.value;
-    if (task !== '') {
-        let number = getNumber();
-        addTask(task, number)
-        taskInput.value = ''; 
-    }
-}
 
 function addTask(task, number){
   const newItem = `<li>
@@ -68,42 +25,80 @@ function addTask(task, number){
   saveTask(task,number)
 }
 
+function saveTask(task,number){
+  let tasks = JSON.parse(sessionStorage.getItem('tasks')) || {};
+  tasks[number]= task;
+  sessionStorage.setItem('tasks',JSON.stringify(tasks));
+  sessionStorage.setItem('highNumber',JSON.stringify(number));
+  number++;
+  
+}
+
+
+addButton.addEventListener('click', addInput);
+function addInput() {
+    let task = taskInput.value;
+    if (task !== '') {
+        let number = getNumber();
+        addTask(task, number)
+        taskInput.value = ''; 
+    }
+}
+
+function getNumber(){
+  let number = JSON.parse(sessionStorage.getItem('highNumber')) || 0;
+  number = parseInt(number)
+  return number + 1 ;
+}
+
 function enterAdd(event){
   if (event.key==='Enter'){
     addInput()
   }
 }
 
+
+
 taskList.addEventListener('click', (event) => {
   const target = event.target;
-  const taskTextDiv = target.parentElement.parentElement.querySelector('.taskText');
 
   if (target.classList.contains('editButton')) {
-    // Handle the edit button click
+    let taskTextDiv = target.parentElement.parentElement.querySelector('.taskText');
     let number = taskTextDiv.getAttribute('data-number');
-    let liElement = target.parentElement.parentElement
+    // let liElement = target.parentElement.parentElement
+    const liElement = target.closest('li'); // Find the parent <li> element
     const currentText = taskTextDiv.textContent;
     let editBar = `
     <input type="text" class="editInput" value="${currentText}" >
     <button class="applyButton" data-number=${number}>Apply</button>
-`
+    `
     liElement.innerHTML = editBar
-  } else if (target.classList.contains('deleteButton')) {
+  } 
+  else if (target.classList.contains('deleteButton')) {
+    let taskTextDiv = target.parentElement.parentElement.querySelector('.taskText');
     let number = taskTextDiv.getAttribute('data-number');
-    let liElement = target.parentElement.parentElement
-    const taskItem = target.closest('li'); // Find the parent <li> element
-    if (taskItem) {
-      taskItem.remove();
+    // let liElement = target.parentElement.parentElement
+    const liElement = target.closest('li'); // Find the parent <li> element
+    if (liElement) {
+      liElement.remove();
     delTask(number);
-  }} else if (target.classList.contains('applyButton')) {
+  }} 
+  else if (target.classList.contains('applyButton')) {
     let number = target.getAttribute('data-number');
-    let liElement = target.parentElement
-    const taskItem = target.closest('li'); // Find the parent <li> element
+    // let liElement = target.parentElement
+    const liElement = target.closest('li'); // Find the parent <li> element
     let inputBar = liElement.querySelector('input')
     let editedItem=inputBar.value
     editTask(editedItem, number,liElement)
   }
 });
+
+function delTask(number){
+  let tasks = JSON.parse(sessionStorage.getItem('tasks'));
+  delete tasks[number];
+  sessionStorage.setItem('tasks',JSON.stringify(tasks));
+  
+  }
 
 function editTask(newTask,number,liElement){
     let editedItem = `
@@ -117,3 +112,8 @@ function editTask(newTask,number,liElement){
     liElement.innerHTML = editedItem
     saveTask(newTask,number)
 }
+
+
+
+
+
